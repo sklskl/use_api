8f6b5046d733bb1ce84893a48d8017a21e23a636
sunkeling <454541398@qq.com>
2022-12-01 23:18:34 +0800

    处理响应字典

--- demo01/python_repos.py
+++ demo01/python_repos.py
@@ -5,4 +5,13 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f'Status code:{r.status_code}')
 response_dict = r.json()
-print(response_dict.values())
+print(f"total repositories:{response_dict['total_count']}")
+# 探索有关仓库的信息
+repo_dicts = response_dict['items']
+print(f"Respositories returned:{len(repo_dicts)}")
+# 研究第一个仓库
+repo_dict = repo_dicts[0]
+print(f"\nKeys:{len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
+print(response_dict.keys())
